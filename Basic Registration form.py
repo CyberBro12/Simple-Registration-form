c7e295d5e1da96005a343063d4205b4bf4587fb5
CyberBro12 <94747680+CyberBro12@users.noreply.github.com>
2024-02-28 21:42:46 +0530

    Basic Registration form.py

--- Basic Registration form.py
+++ Basic Registration form.py
@@ -71,23 +71,3 @@ while True:
 
     else:
         print("Wrong option!")
-
-import socket
-
-# Set up server socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 8888))  # Change IP and port as needed
-server_socket.listen(5)
-
-print("Server listening...")
-
-while True:
-    client_socket, addr = server_socket.accept()
-    print(f"Connection from {addr} has been established.")
-
-    filename = 'Save.txt'  # Replace with the file you want to send
-    with open(filename, 'rb') as file:
-        file_data = file.read()
-
-    client_socket.sendall(file_data)
-    client_socket.close()
